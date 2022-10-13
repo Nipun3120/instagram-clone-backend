6de5ed818fd33138c4ed44d6915b9fdcbfd2c91f
Nipun Verma <Nipunverma39@gmail.com>
2022-10-13 22:36:11 +0530

    setup

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./middlewares/error");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/public", express.static("public"));
+
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config({ path: "config/config.env" });
+}
+
+// // import routes
+app.use(require("./routes"));
+
+// deployment
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server is Running! 🚀");
+  });
+}
+
+// error middleware
+app.use(errorMiddleware);
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,70 @@
+const app = require("./app");
+require("./config/database").connect();
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server Running on http://localhost:${PORT}`);
+});
+
+// socket.io
+
+const io = require("socket.io")(server, {
+  cors: { origin: "*" },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("User Connected !!");
+  // console.log(users);
+
+  // get userId and socketId from client
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  // get and send message
+  socket.on("sendMessage", ({ senderId, receiverId, content }) => {
+    const user = getUser(receiverId);
+
+    io.to(user?.socketId).emit("getMessage", {
+      senderId,
+      content,
+    });
+  });
+
+  // typing states
+  socket.on("typing", ({ senderId, receiverId }) => {
+    const user = getUser(receiverId);
+    console.log(user);
+    io.to(user?.socketId).emit("typing", senderId);
+  });
+
+  socket.on("typing stop", ({ senderId, receiverId }) => {
+    const user = getUser(receiverId);
+    io.to(user?.socketId).emit("typing stop", senderId);
+  });
+
+  // user disconnected
+  socket.on("disconnect", () => {
+    console.log("⚠️ Someone disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+
+    console.log(users);
+  });
+});
